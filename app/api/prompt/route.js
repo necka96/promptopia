b9a5cc2942161980b7278248348867da35f4e3f3
Nemanja Djordjevic <76485890+necka96@users.noreply.github.com>
2024-01-13 20:02:49 +0100

    fix route.js

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,5 +1,5 @@
 import Prompt from "@models/prompt";
-import { connectToDB } from "@utils/database";
+import { connectToDb  } from "@utils/database";
 export const dynamic = "force-dynamic";
 export const GET = async (request) => {
     try {
